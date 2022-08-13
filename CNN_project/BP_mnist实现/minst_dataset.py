a46f9fc44e17f4bd5615ca5ae85aca795c3cc189
zhcanran <1215355076@qq.com>
2022-08-13 22:44:50 +0800

    只是练习

--- CNN_project/BP_mnist实现/minst_dataset.py
+++ CNN_project/BP_mnist实现/minst_dataset.py
@@ -27,7 +27,8 @@ class Mnist_dataset:
         img, label = (data[2], data[3]) if train else (data[0], data[1])
         self.datas = self.load_imges(os.path.join(path, img))
         self.labels = self.load_labels(os.path.join(path, label))
-        self.datas=self.datas/255-0.5
+        self.labels=self.one_hot(self.labels,10)
+        self.datas = self.datas / 255 - 0.5
 
     def __getitem__(self, item):
         return self.datas[item], self.labels[item]
@@ -41,30 +42,37 @@ class Mnist_dataset:
             data = f.read()
         row, cols = struct.unpack('>ii', data[8:16])
         data = np.asarray(bytearray(data[16:]), dtype=np.float32)
-        data = data.reshape((-1, row*cols))
+        data = data.reshape((-1, row * cols))
         return data
 
     @staticmethod
     def load_labels(path):
         with open(path, 'rb') as f:
             labels = f.read()
-        labels = np.asarray(bytearray(labels[8:]), dtype=np.float32)
+        labels = np.asarray(bytearray(labels[8:]), dtype=np.float32).reshape(-1,1)
         return labels
+
+    @staticmethod
+    def one_hot(labels, classifi):
+        classifi = np.arange(classifi)
+        return (labels == classifi).astype(np.float32)
 
 
 if __name__ == '__main__':
-    # # print(np.array([1,2,3]))
-    # with open(train_images_path, 'rb') as f:
-    #     train_data = f.read()
-    # #< > 指的是读取顺序
-    # # print(hex(struct.unpack('>i', train_data[:4])[0]))#16进制与上述注释一致
-    # # data = struct.unpack('>iiii', train_data[:16])  # i代表一个数-->4个字符
-    #
-    # data=np.asarray(bytearray(train_data[16:]),dtype=np.int8)
-    # print(data)
-    path = r'D:\project\CNN_project\jobwork\jobwork02\data\MNIST\raw'
+# # print(np.array([1,2,3]))
+# with open(train_images_path, 'rb') as f:
+#     train_data = f.read()
+# #< > 指的是读取顺序
+# # print(hex(struct.unpack('>i', train_data[:4])[0]))#16进制与上述注释一致
+# # data = struct.unpack('>iiii', train_data[:16])  # i代表一个数-->4个字符
+#
+# data=np.asarray(bytearray(train_data[16:]),dtype=np.int8)
+# print(data)
+    path = r'D:\project\data\mnist\MNIST\raw'
     data = Mnist_dataset(path)
-    #
-    # for i in data:
-    #     print(i)
-    #     break
+
+    for i in data:
+        print(i)
+        break
+
+
